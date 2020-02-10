4f31aa1a6e15a31c5facac9fe3a7224fef551ac4
Andrewjhrussell <andrewjhrussell@gmail.com>
2020-02-09 21:16:47 -0500

    passwordGeneratorHomeWork

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,18 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pw1 = ["A","B","C","D","E","F","G","H"];
+var pw2 = ["a","b","c","d","e","f","g","h"];
+  var pw3 = [1,2,3,4,5,6,7,8];
+  var pw4 = ["!","@","#","$","%","^","&","*"];
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function generatePassword () {
+ 
+var selectpasswords = ""
+var answer = ""
+
+var user = prompt ("length of password")
+
+var pw3 = confirm("number?");
+var pw2 = confirm("lowercase letters");
+var pw1 = confirm("uppercase letters");
+var pw4 = confirm("symbols");
+
+if (user < 8||user > 128) {alert("invalid selection");
+return generatePassword();}
+
+if(pw1){selectpasswords= selectpasswords + pw1}
+
+if(pw2){selectpasswords= selectpasswords + pw2}
+
+if(pw4){selectpasswords= selectpasswords + pw4}
+
+if(pw3){selectpasswords= selectpasswords + pw3}
+
+if (pw2 === false && pw3 === false && pw1 === false && pw4 === false) {
+    alert ("retry");
+    return generatePassword();
+  }
+
+  for(var count = 1; count <= userchoice; count++) {
+    var randomIndex = Math.floor(Math.random() * allSelectTypes.length);
+    result = result + selectpasswords.charAt(randomIndex)
+
+
+  
   var passwordText = document.querySelector("#password");
 
-  for(var i = 0; i <= complexity; i++){
-	password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  
   
 
-  passwordText.value = ("ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+");
+  passwordText.value = password;
 
-}
+}}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click",writePassword);
